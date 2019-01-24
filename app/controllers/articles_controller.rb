37b0d0722295eda58b54ad053efce9063bdd8ced
randomsystemglitch <randomsystemglitch@gmail.com>
2019-01-24 04:51:05 -0500

    edit added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,15 @@
 class ArticlesController < ApplicationController
 
+  
+  
+  
+  def index
+    @articles = Article.all
+  end
+  
+  
+  
+  
   def new
     @article = Article.new
   end
@@ -15,14 +25,28 @@ class ArticlesController < ApplicationController
       render 'new'
     end            
   end
-
   
-  def show
-    
-    @article = Article.find(params[:id])
-    
+  
+  def edit
+  @article = Article.find(params[:id])
   end
   
+
+  
+  def show   
+    @article = Article.find(params[:id])
+  end
+  
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
   
   private
     def article_params
