961fd94d099f527552757fdf5636143971f0daca
randomsystemglitch <randomsystemglitch@gmail.com>
2019-01-25 10:34:49 -0500

    Intalled bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :articles
+  
+  #root "pages#show", page: "pages/home"
 end
 
 
