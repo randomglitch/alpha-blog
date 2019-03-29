099639a80121c1c3fb7000e16a5b545d84f96945
randomsystemglitch <randomsystemglitch@gmail.com>
2019-03-29 12:07:48 -0400

    Created homepage and added image

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :articles
-  
-  #root "pages#show", page: "pages/home"
+  root to: redirect('pages/home')
+  get "/pages/:page" => "pages#show"
+
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_08_160739) do
+ActiveRecord::Schema.define(version: 2019_03_21_151621) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "homepages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
