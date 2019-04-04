3e48807d8bebf4fc39a1857f0b05c9fdf2dc0ec8
randomsystemglitch <randomsystemglitch@gmail.com>
2019-04-04 06:43:44 -0400

    created users table & model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_151621) do
+ActiveRecord::Schema.define(version: 2019_04_04_102806) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2019_03_21_151621) do
   end
 
   create_table "homepages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
